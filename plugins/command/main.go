e28d42160bad81ed33065a43d59c1bff2b13c831
Tom Steele <thomasjsteele@gmail.com>
2017-02-08 23:28:52 -0800

    Adds command payload type

--- plugins/command/main.go
+++ plugins/command/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"strings"
+)
+
 // Name is a descriptive name of the
 func Name() string {
 	return "command"
@@ -15,7 +19,17 @@ func Help() string {
 	return "command <command>"
 }
 
-// Generate generates a payload based on the arguments provided
-func Generate(args []string) ([]byte, error) {
+// String returns a string fiendly version of the command.
+func String(data []byte) string {
+	return string(data)
+}
 
+// Generate generates a payload based on the arguments provided
+func Generate(data []byte) ([]byte, error) {
+	return data, nil
+}
+
+// Process processes arguments and creates the stored payload.
+func Process(args []string) ([]byte, error) {
+	return []byte(strings.Join(args, " ")), nil
 }
